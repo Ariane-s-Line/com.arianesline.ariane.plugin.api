f9c27b3a92bcbd964b1fe10eb9bd02d394c8a29f
Jonathan Dekhtiar <jonathan@dekhtiar.com>
2025-06-22 12:04:06 -0400

    Import fix

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,9 @@
 module com.arianesline.ariane.plugin.api {
 
     exports com.arianesline.ariane.plugin.api;
-    requires com.arianesline.cavelib.api;
-    requires javafx.graphics;
+    requires transitive com.arianesline.cavelib.api;
+    requires transitive javafx.graphics;
+    requires transitive javafx.base;
+    requires transitive javafx.fxml;
 
 }
